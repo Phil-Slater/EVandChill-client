17cc3c83fcfc5ac638dffdc07a59c1503ca01c9e
jon-cundiff <jon.cundiff@gmail.com>
2022-03-22 13:44:59 -0500

    adds large logo to homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import logoLg from "./evandchill-HI.png";
 const HomePage = () => {
     return (
         <div>
             <h1>EV and Chill</h1>
+            <img src={logoLg} alt="EV & Chill Logo" />
         </div>
     );
 };
