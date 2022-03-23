05ba3080749b07e367e63f3c79884895c6e37f0c
Katie Freeman <katiefreeman@Carltons-MacBook-Pro.local>
2022-03-23 10:36:58 -0400

    implementing login and register components

---
+++ src/components/auth/Register.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function Register(props) {
+  const [user, setUser] = useState([]);
+  const handleTextChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleRegister = async () => {
+    const response = await fetch("http://localhost:8080/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    const responseJson = await response.json();
+    if (responseJson.error) {
+        console.log(responseJson.error)
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <input
+        type="text"
+        onChange={handleTextChange}
+        placeholder="Enter Username"
+        name="username"
+      />
+      <input
+        type="email"
+        onChange={handleTextChange}
+        placeholder="Enter Email"
+        name="email"
+      />
+      <input
+        type="password"
+        onChange={handleTextChange}
+        placeholder="Enter Password"
+        name="password"
+      />
+      <div>
+        <button onClick={handleRegister}>Register</button>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
