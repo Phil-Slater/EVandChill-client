1a4018ca6eb90ff14b2948a4ce23c32c2091b3a0
jon-cundiff <jon.cundiff@gmail.com>
2022-03-22 19:01:54 -0500

    fixes mobile header gap, limits logo size on homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import logoLg from "./evandchill-HI.png";
+
+import "./HomePage.css";
 const HomePage = () => {
     return (
         <div>
             <h1>EV and Chill</h1>
-            <img src={logoLg} alt="EV & Chill Logo" />
+            <img src={logoLg} alt="EV & Chill Logo" className="hp-logo" />
         </div>
     );
 };
