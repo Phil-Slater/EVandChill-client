33fb585e36f73993dc8e49a4cbccc8ff5f80fa23
Katie Freeman <katiefreeman@carltons-mbp.lan>
2022-03-23 22:17:32 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Login from './components/auth/Login';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>TEST</h1>
+      <Login/>
     </div>
   );
 }
