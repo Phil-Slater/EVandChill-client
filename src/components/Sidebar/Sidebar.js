2ad6bdaa12de14e6341166f070ed02997f78912c
jon-cundiff <jon.cundiff@gmail.com>
2022-03-24 10:27:53 -0500

    adds sidebar placement and bg styling

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import "./Sidebar.css";
 
 const Sidebar = () => {
+    const isMenuActive = useSelector((state) => state.display.isMenuActive);
     return (
-        <div className="sidebar-menu">
+        <div className={`sidebar-menu ${isMenuActive ? "active" : ""}`}>
             <p>Hello</p>
             <p>Hello</p>
             <p>Hello</p>
