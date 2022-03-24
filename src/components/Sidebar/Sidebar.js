fdab83d183328c0c3541f6b09b810cd3d6cf521d
jon-cundiff <jon.cundiff@gmail.com>
2022-03-24 11:13:28 -0500

    builds out sidebar menu links

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 import "./Sidebar.css";
+import UserlessLinks from "./UserlessLinks";
+import UserLinks from "./UserLinks";
 
 const Sidebar = () => {
     const isMenuActive = useSelector((state) => state.display.isMenuActive);
+    const user = useSelector((state) => state.auth.user);
+
     return (
         <div className={`sidebar-menu ${isMenuActive ? "active" : ""}`}>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
+            <h1>EV & Chill</h1>
+            <Link to="/">Home</Link>
+            <Link to="/stations">Stations Near Me</Link>
+            {user ? <UserLinks user={user} /> : <UserlessLinks />}
+            <hr />
+            <Link to="/settings" className="sidebar-settings">
+                Settings
+            </Link>
+
+            <div className="sidebar-footer">
+                <p>Created March 2022</p>
+                <p>
+                    <Link to="/about">About Us</Link>
+                </p>
+            </div>
         </div>
     );
 };
