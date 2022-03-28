66b086baadbbfdc30ee8e9afbefb199a82d04803
jon-cundiff <jon.cundiff@gmail.com>
2022-03-28 13:39:37 -0500

    creates initial styling for profile page

--- src/components/Profile/Favorites.js
+++ src/components/Profile/Favorites.js
@@ -1,27 +1,31 @@
 import React from "react";
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux";
 import { handleDeleteFavorite } from "../../util/axiosConfig";
-
 
 function Favorites() {
     const user = useSelector((state) => state.auth.user);
-    
+
     const handleRemove = async (userId, favoriteId) => {
+        handleDeleteFavorite(userId, favoriteId);
+    };
 
-      handleDeleteFavorite(userId, favoriteId);
-    }
-
-  return (
-    <div className="favorites">
-      <h3>Favorites</h3>
-      {user.favorites.map((favorite) => (
-        <li key={favorite._id}>{
-            favorite.stationId}
-            <button onClick={()=>handleRemove(user._id,favorite._id)}>Remove</button> 
-            </li>
-      ))}
-    </div>
-  );
-};
+    return (
+        <div className="profile-favorites">
+            <h3>Favorites</h3>
+            <ul>
+                {user.favorites.map((favorite) => (
+                    <li key={favorite._id}>
+                        {favorite.stationId}
+                        <button
+                            onClick={() => handleRemove(user._id, favorite._id)}
+                        >
+                            Remove
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
 
 export default Favorites;
