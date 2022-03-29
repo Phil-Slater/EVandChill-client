ae9e683f7e697d5f38420bf940d559ee803fd263
jon-cundiff <jon.cundiff@gmail.com>
2022-03-29 10:41:03 -0500

    adds info windows to results map markers

--- src/components/Search/MapMarker.js
+++ src/components/Search/MapMarker.js
@@ -1,11 +1,25 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { renderToString } from "react-dom/server";
+import InfoBox from "./InfoBox";
 
-const MapMarker = (options) => {
+const MapMarker = ({ station, map, position, icon }) => {
     const [marker, setMarker] = useState();
+    const options = { map, position, icon };
 
     useEffect(() => {
         if (!marker) {
             setMarker(new window.google.maps.Marker(options));
+        } else if (marker && station) {
+            const infoWindow = new window.google.maps.InfoWindow({
+                content: renderToString(<InfoBox station={station} />),
+            });
+            marker.addListener("click", () => {
+                infoWindow.open({
+                    anchor: marker,
+                    map: map,
+                    shouldFocus: false,
+                });
+            });
         }
 
         return () => {
