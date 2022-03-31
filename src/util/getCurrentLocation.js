b1bda6b74d35dd6b470a475cc85e765f39bc8dab
jon-cundiff <jon.cundiff@gmail.com>
2022-03-31 14:41:18 -0500

    implements fix to allow new search results to display on map if on that page

--- src/util/getCurrentLocation.js
+++ src/util/getCurrentLocation.js
@@ -1,12 +1,21 @@
 const getPosition = () => {
     return new Promise((res, rej) => {
-        navigator.geolocation.getCurrentPosition(res, rej)
-    })
-}
+        navigator.geolocation.getCurrentPosition(res, rej);
+    });
+};
 
 const getCurrentLocation = async () => {
-    const position = await getPosition()
-    return position
-}
+    let position = JSON.parse(localStorage.getItem("position"));
+    if (!position || Date.now() - position.timestamp > 1800000) {
+        position = await getPosition();
+        const { timestamp } = position;
+        const { latitude, longitude } = position.coords;
+        localStorage.setItem(
+            "position",
+            JSON.stringify({ coords: { latitude, longitude }, timestamp })
+        );
+    }
+    return position;
+};
 
-export default getCurrentLocation
+export default getCurrentLocation;
