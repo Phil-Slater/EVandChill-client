035cffafe91d154b5141fad3e41f9a1bc0dbe8b8
Phil-Slater <pdsltr@gmail.com>
2022-04-02 21:19:02 -0400

    update styling on AddReview

--- src/components/Search/AddReview.js
+++ src/components/Search/AddReview.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { postAddReview } from "../../util/axiosConfig";
 import { useParams } from 'react-router-dom';
@@ -6,37 +6,38 @@ import StarRatingComponent from 'react-star-rating-component';
 
 
 const AddReviews = () => {
-    const [isWorking, setIsworking] = useState(true)
-    const [review, setReview] = useState(" ")
-    const [rating, setRating] = useState(0)
-    const user = useSelector((state) => state.auth.user);
-    const { stationId } = useParams();
-    console.log('STATION ID: ', stationId)
-    
+  const [isWorking, setIsworking] = useState(true)
+  const [review, setReview] = useState(" ")
+  const [rating, setRating] = useState(0)
+  const user = useSelector((state) => state.auth.user);
+  const { stationId } = useParams();
+  console.log('STATION ID: ', stationId)
+
   return (
     <div className="add-reviews">
-      <h2>Add review:</h2>
+      <h2 style={{ fontSize: "1.75rem" }}>Add review:</h2>
       <div>
-        <h3>rating:</h3>
+        <h3 style={{ fontSize: "1.5rem" }}>Rating:</h3>
         <StarRatingComponent
           name="rating"
           starCount={5}
           value={rating}
           renderStarIcon={() => (
-            <span style={{ fontSize: "1.3em", fontWeight: "bold" }}>ϟ</span>
+            <span style={{ fontSize: "3em", fontWeight: "bold" }}>ϟ</span>
           )}
           onStarClick={(value) => setRating(value)}
         />
       </div>
-      <h3>Add a comment:</h3>
-      <textarea onChange={(e) => setReview(e.target.value)} name="review" />
+      <h3 style={{ fontSize: "1.5rem" }}>Add a comment:</h3>
+      <textarea onChange={(e) => setReview(e.target.value)} name="review" className="review-text" />
       <p>
-        Working
+        <span style={{ fontSize: "1.25rem" }}>Working?</span>
         <input
           type="checkbox"
           name="isWorking"
           checked={isWorking}
           onChange={() => setIsworking(!isWorking)}
+          style={{ width: "40px", height: "20px" }}
         />
       </p>
       <button
@@ -53,8 +54,8 @@ const AddReviews = () => {
       >
         Add Review
       </button>
-    </div>
+    </div >
   );
-}; 
+};
 
 export default AddReviews;
